72d21029693a4e26a2103933ab15f1aadf82638d
Brendon Huang <brendonhuang12@gmail.com>
2026-04-27 15:40:18 -0400

    Initialize Firebase in main app entry point

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+
+import 'firebase_options.dart';
 import 'screens/chat_screen.dart';
 import 'screens/dashboard_screen.dart';
 import 'screens/login_screen.dart';
@@ -7,9 +10,18 @@ import 'screens/study_groups_screen.dart';
 import 'screens/study_planner_screen.dart';
 import 'screens/study_room_finder_screen.dart';
 import 'screens/timer_screen.dart';
+import 'services/notification_service.dart';
 import 'theme/study_n_sync_theme.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  await NotificationService().initialize();
+
   runApp(const MyApp());
 }
 
@@ -27,7 +39,8 @@ class MyApp extends StatelessWidget {
         LoginScreen.routeName: (context) => const LoginScreen(),
         DashboardScreen.routeName: (context) => const DashboardScreen(),
         StudyPlannerScreen.routeName: (context) => const StudyPlannerScreen(),
-        StudyRoomFinderScreen.routeName: (context) => const StudyRoomFinderScreen(),
+        StudyRoomFinderScreen.routeName: (context) =>
+            const StudyRoomFinderScreen(),
         StudyGroupsScreen.routeName: (context) => const StudyGroupsScreen(),
         ChatScreen.routeName: (context) => const ChatScreen(),
         TimerScreen.routeName: (context) => const TimerScreen(),
